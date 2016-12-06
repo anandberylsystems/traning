94d647b6f61efa6ab8c2a6d27b0b0431149f0587
beryl systems <anand.kumar@berylsystems.com>
2016-12-06 19:03:27 +0530

    rails task for delete and insert data

--- library/app/controllers/home_controller.rb
+++ library/app/controllers/home_controller.rb
@@ -30,4 +30,58 @@ class HomeController < ApplicationController
 		@publication = Publication.all
 	end
 
+	#------------------insert data into tables:--------------#
+
+	 # def insert_to_students
+	 # end
+
+	 # def insert_to_staffs
+	 # end
+
+	 # def insert_to_books
+	 # end
+
+	 # def insert_to_borrowers
+	 # end
+
+	 # def insert_to_departments
+	 # end
+
+	#---------insert into database-------
+
+	def create_student
+
+	Student.create(name: params[:name], sex: params[:gender], dob: params[:dob], dept_id: params[:deptid], contact: params[:contact])
+	 	redirect_to '/Student_details'
+	end
+
+	 def create_books
+	 	Book.create(title: params[:title], language: params[:language], pubId: params[:pub_id], deptId: params[:dept_id], actual_copy: params[:actualcopy], current_copy: params[:currentcopy])
+         redirect_to '/book_details'		
+	 end
+
+
+	def create_department
+		Department.create(name: params[:deptname])
+		redirect_to '/Department_details'
+	end
+
+	def create_borrowers
+		Borrower.create(book_id: params[:book_id], return_date: params[:return_date], issued_id: params[:issued_id], st_id: params[:st_id])
+		redirect_to '/borrower_details'
+	end
+
+	def create_staffs
+		staff.create(name: params[:name], contact: params[:contact])
+		redirect_to '/staff_details'
+	end
+
+
+	#...........delte one row.....
+
+	def delete_staff
+		@staff = Staff.find(params[:id])
+		@staff.destroy
+	end
+
 end
--- library/config/routes.rb
+++ library/config/routes.rb
@@ -3,9 +3,21 @@ Rails.application.routes.draw do
    get '/book_details', to: 'home#book'
    get '/borrower_details', to: 'home#borrower'
    get '/staff_details', to: 'home#staff'
-   get '/Department details', to: 'home#department'
+   get '/Department_details', to: 'home#department'
    get '/Student_details', to: 'home#student'
    get '/publication_details', to: 'home#publication'
+   get '/insert_to_students', to: 'home#insert_to_students'
+   get '/insert_to_staffs', to: 'home#insert_to_staffs'
+   get '/insert_to_books', to: 'home#insert_to_books'
+   get '/insert_to_borrowers', to: 'home#insert_to_borrowers'
+   get '/insert_to_departments', to: 'home#insert_to_departments'
+   post '/create_department', to: 'home#create_department'
+   post '/create_student', to:'home#create_student'
+   post '/create_book', to:'home#create_books'
+   post '/create_borrower', to:'home#create_borrowers'
+   post '/create_staff', to: 'home#create_staffs'
+   delete '/delete_staff', to: 'home#delete_staff'
+   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
